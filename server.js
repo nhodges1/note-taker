eb7077115ff89a4dea8bee493bd1f01e1ca1a5c2
nhodges <hodges27@outlook.com>
2022-02-20 16:58:14 -0500

    localhost

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+// Initialize the app and create a port
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Start the server on the port
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
