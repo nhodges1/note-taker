ebc53cfdf50a6e3d4a9e6cb0c747497bd4470184
nhodges <hodges27@outlook.com>
2022-02-21 16:20:55 -0500

    storeupdated

---
+++ db/store.js
@@ -0,0 +1,37 @@
+const util = require('util');
+const fs = require('fs');
+
+// generates unique ids
+const uuidv1 = require('uuid/v1');
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Store {
+    read() {
+        return readFileAsync('db/db.json', 'utf8');
+    }
+
+    write(note) {
+        return writeFileAsync('db/db.json', JSON.stringify(note));
+    }
+
+    getNotes() {
+        return this.read().then((notes) => {
+            let parsedNotes;
+
+            // sends back a new empty array if notes isn't an array or can't be turned into one
+            try {
+                parsedNotes = [].concat(JSON.parse(notes));
+            } catch (err) {
+                parsedNotes = [];
+            }
+
+            return parsedNotes;
+        });
+    }
+
+    addNote(note) {
+        
+    }
+}
